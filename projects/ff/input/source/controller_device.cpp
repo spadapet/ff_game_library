e6f8be5a3e813f29c1a23e9a88dda027d2f49ccf
Peter Spada <peterspa@microsoft.com>
2021-01-20 15:44:56 -0800

    Start controller

---
+++ projects/ff/input/source/controller_device.cpp
@@ -0,0 +1,3 @@
+#include "pch.h"
+#include "controller_device.h"
+#include "input.h"
--- projects/ff/input/source/controller_device.h
+++ projects/ff/input/source/controller_device.h
@@ -4,11 +4,41 @@
 
 namespace ff
 {
+    enum class controller_direction_type
+    {
+        left_stick,
+        right_stick,
+        direction_pad,
+    };
+
+    enum class controller_trigger_type
+    {
+        left_trigger,
+        right_trigger,
+    };
+
     class controller_device : public input_device_base
     {
     public:
-        controller_device(size_t index);
+#if UWP_APP
+        using index_type = typename Windows::Gaming::Input::Gamepad^;
+#else
+        using index_type = typename size_t;
+#endif
+        controller_device(index_type index);
         virtual ~controller_device() override;
+
+        index_type index() const;
+        void index(index_type index);
+
+        ff::point_float direction_pressing(controller_direction_type type, bool digital) const;
+        ff::rect_int direction_press_count(controller_direction_type type) const;
+
+        float trigger_pressing(controller_trigger_type type, bool digital) const;
+        int trigger_press_count(controller_trigger_type type) const;
+
+        bool button_pressing(int vk) const;
+        int button_press_count(int vk) const;
 
         // input_device_base
         virtual void advance() override;
@@ -17,10 +47,8 @@ namespace ff
         virtual ff::signal_sink<const ff::input_device_event&>& event_sink() override;
         virtual void notify_main_window_message(ff::window_message& message) override;
 
-        size_t index() const;
-
     private:
-        size_t index_;
+        index_type index_;
         ff::signal<const ff::input_device_event&> device_event;
     };
 }
