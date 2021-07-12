0a3b540a1bd2502c8dbeacc9d440d2bef5d6c42c
Peter Spada <peterspa@microsoft.com>
2021-07-12 05:02:54 -0700

    ToolsVersion=Current

--- projects/ff/graphics/source/buffer.h
+++ projects/ff/graphics/source/buffer.h
@@ -7,9 +7,12 @@ namespace ff
     class buffer : public ff::internal::graphics_child_base
     {
     public:
+#if DXVER == 11
         buffer(D3D11_BIND_FLAG type);
         buffer(D3D11_BIND_FLAG type, size_t size);
         buffer(D3D11_BIND_FLAG type, std::shared_ptr<ff::data_base> initial_data, bool writable);
+#elif DXVER == 12
+#endif
         buffer(buffer&& other) noexcept = default;
         buffer(const buffer& other) = delete;
         virtual ~buffer() override;
@@ -18,14 +21,17 @@ namespace ff
         buffer& operator=(const buffer & other) = delete;
         operator bool() const;
 
+#if DXVER == 11
         D3D11_BIND_FLAG type() const;
+        ID3D11Buffer* dx_buffer() const;
+#elif DXVER == 12
+#endif
         size_t size() const;
         bool writable() const;
         void* map(size_t size);
         void unmap();
         bool update_discard(const void* data, size_t size);
         bool update_discard(const void* data, size_t data_size, size_t buffer_size);
-        ID3D11Buffer* dx_buffer() const;
 
         // graphics_child_base
         virtual bool reset() override;
