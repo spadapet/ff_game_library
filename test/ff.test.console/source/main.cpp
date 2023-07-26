773b1fdfe924b5adcbcd385bbadeb8f7f17b5f2d
Peter Spada <spadapet@hotmail.com>
2023-07-26 07:14:56 -0700

    Remove event perf test

--- test/ff.test.console/source/main.cpp
+++ test/ff.test.console/source/main.cpp
@@ -3,15 +3,13 @@
 void run_input_device_events();
 void run_test_app();
 void run_coroutine_app();
-void run_event_perf();
 
 int main()
 {
     std::cout << "Choose:" << std::endl
         << "1) Input device events" << std::endl
         << "2) Test app" << std::endl
-        << "3) co_await in an app" << std::endl
-        << "4) Event creation perf" << std::endl;
+        << "3) co_await in an app" << std::endl;
 
     int choice = 0;
     std::cin >> choice;
@@ -28,10 +26,6 @@ int main()
 
         case 3:
             ::run_coroutine_app();
-            break;
-
-        case 4:
-            ::run_event_perf();
             break;
     }
 
