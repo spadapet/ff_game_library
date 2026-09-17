9f19124201e3c2e52253967798d184564ecaf577
Peter Spada <spadapet@hotmail.com>
2026-09-16 22:35:53 -0400

    Add ff.test.c

---
+++ test/ff.test.c/source/main.c
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+
+int main()
+{
+    printf("Hello World!\n");
+
+    ff_dx12_init_params params = ff_dx12_init_params_default();
+    if (ff_dx12_init(&params))
+    {
+        ff_dx12_device_valid();
+        ff_dx12_destroy();
+    }
+
+    return 0;
+}
